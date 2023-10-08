dd5607809967cefdc8ad70cc483b90883f53f82e
Numbers00 <kylemariangelo@gmail.com>
2023-10-08 16:31:43 +0800

    Upload for submission up to exer 2.01

---
+++ part2/exer2.01/pingpong/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Initialize the counter
+counter = 0
+
+
+@app.route('/pingpong', methods=['GET'])
+def pingpong():
+    global counter
+
+    counter += 1
+
+    response = f"pong {counter}"
+    return jsonify({'message': response})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
